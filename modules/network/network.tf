a141e3e24bf031e7a7fc9f6ecb401ad1ab3bdb80
Joás Garcia <joasdemelo@hotmail.com>
2023-11-05 20:14:44 -0300

    Corrigindo as subnets e tornando-as públicas.

--- modules/network/network.tf
+++ modules/network/network.tf
@@ -2,16 +2,19 @@ resource "aws_subnet" "restaurantPublicSubnet1" {
   vpc_id            = data.aws_vpc.restaurant-vpc.id
   cidr_block        = "10.0.16.0/20"
   availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "restaurantPublicSubnet2" {
   vpc_id            = data.aws_vpc.restaurant-vpc.id
   cidr_block        = "10.0.32.0/20"
   availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "restaurantPublicSubnet3" {
   vpc_id            = data.aws_vpc.restaurant-vpc.id
   cidr_block        = "10.0.48.0/20"
   availability_zone = "us-east-1c"
+  map_public_ip_on_launch = true
 }
